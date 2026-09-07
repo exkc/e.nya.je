bf32a4559a4ebe170f239817a457a775fe4c6d58
exkc <exkc@exkc.moe>
2026-09-07 22:32:25 +0800

    fix link

--- getroot/dualbro.js
+++ getroot/dualbro.js
@@ -434,9 +434,8 @@ Then Click Root the TV :3 to root the tv` ;
  title = 'Connection Failed';
   body = `Cant reach over web socket,Maybe your browser has blocked local ip or self signed ssl cert or both.
 Maybe those link as below can help :
-https://bugzilla.mozilla.org/show_bug.cgi?id=1973932
 https://codeberg.org/celenity/Phoenix/issues/162
-https://help.motorolanetwork.com/kb/general/troubleshooting-connection-isn-t-private-message 
+https://superuser.com/questions/1780337/let-firefox-accept-a-website-with-self-signed-certificate
 ` ;
 
 	}
